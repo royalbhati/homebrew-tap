7cd159cc08da3b23d3193ea4ce93700cec257f69
Royal Bhati <royal240596@gmail.com>
2026-03-31 21:40:13 +0530

    Fix SHA256 for notarized build

--- Casks/hoptab.rb
+++ Casks/hoptab.rb
@@ -1,18 +1,18 @@
 cask "hoptab" do
-    version "1.2.0"
-    sha256 "8112911ea2aae5eea9ba5f27000159567994e079c1e0bcfca0e71ac4b3dc3a45"
+  version "1.2.0"
+  sha256 "c2829f2e06412d3315c852edb629a9045da341788b260aee604f53b067cc150a"
 
-    url "https://github.com/royalbhati/HopTab/releases/download/v#{version}/HopTab-#{version}.zip"
-    name "HopTab"
-    desc "The workspace manager macOS should have shipped with"
-    homepage "https://github.com/royalbhati/HopTab"
+  url "https://github.com/royalbhati/HopTab/releases/download/v#{version}/HopTab-#{version}.zip"
+  name "HopTab"
+  desc "The workspace manager macOS should have shipped with"
+  homepage "https://github.com/royalbhati/HopTab"
 
-    depends_on macos: ">= :sonoma"
+  depends_on macos: ">= :sonoma"
 
-    app "HopTab.app"
+  app "HopTab.app"
 
-    zap trash: [
-      "~/Library/Preferences/com.royal.HopTab.plist",
-      "~/Library/Application Support/HopTab",
-    ]
-  end
+  zap trash: [
+    "~/Library/Preferences/com.hoptab.app.plist",
+    "~/Library/Application Support/HopTab",
+  ]
+end
